66a622cf88696f0b2254a901d869087828e3fd59
popoversail <151128548+popoversail@users.noreply.github.com>
2023-11-18 23:30:09 +0800

    popover: 洛谷1271

--- hahahaha/1.cpp
+++ hahahaha/1.cpp
@@ -1,2 +1,21 @@
-ongtviygkg
-lkn
+#include <iostream>
+#include <algorithm>  
+using namespace std;
+
+int main() {
+    int n,m;
+    cin >> n >> m;
+    int a[m];
+
+    for(int i = 0; i < m; i++) {
+        cin >> a[i];
+    }
+
+    sort(a,a + m);  
+
+    for(int i = 0; i < m; i++) {
+        cout << a[i] << ' ';
+    }
+
+    return 0;
+}
