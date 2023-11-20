892f7979d90502a8a5cce03f7c7e4f01b38a1d39
bfs1201 <146979061+bfs1201@users.noreply.github.com>
2023-11-20 21:57:58 +0800

    Codeforces Round 910 (Div. 2)
    B - Milena and Admirer

---
+++ dislike/CFMilena and Admirer.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cmath>
+#include <vector>
+#include <algorithm>
+using namespace std;
+typedef long long ll; 
+
+int main() {
+    int t; cin >> t; 
+    while(t--) {
+        int n; cin >> n; 
+        vector<int> nums(n); 
+        for(int& a: nums) cin >> a; 
+        int sign = nums.back(); 
+        ll ans = 0; 
+        for(int i = n - 2; i >= 0; --i) {
+            int cnt = (nums[i] - 1) / sign; 
+            ans += cnt; 
+            sign = nums[i] / (cnt + 1); 
+        }
+        cout << ans << endl; 
+    }
+    return 0;
+}
