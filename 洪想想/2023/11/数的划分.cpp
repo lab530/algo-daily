16a21651ca7ffc8eb4319096349e52f1db74c729
xfwmhxx <2015870900@qq.com>
2023-11-22 21:35:06 +0800

    洪想想：洛谷 1025
    # [NOIP2001 提高组] 数的划分
    
    ## 题目描述
    
    将整数 $n$ 分成 $k$ 份，且每份不能为空，任意两个方案不相同（不考虑顺序）。
    
    例如：$n=7$，$k=3$，下面三种分法被认为是相同的。
    
    $1,1,5$;
    $1,5,1$;
    $5,1,1$.
    
    问有多少种不同的分法。
    
    ## 输入格式
    
    $n,k$ （$6<n \le 200$，$2  \le k  \le  6$）
    
    ## 输出格式
    
    $1$ 个整数，即不同的分法。
    
    ## 样例 #1
    
    ### 样例输入 #1
    
    ```
    7 3
    ```
    
    ### 样例输出 #1
    
    ```
    4
    ```
    
    ## 提示
    
    四种分法为：
    $1,1,5$;
    $1,2,4$;
    $1,3,3$;
    $2,2,3$.
    
    **【题目来源】**
    
    NOIP 2001 提高组第二题

---
+++ 洪想想/2023/11/数的划分.cpp
@@ -0,0 +1,21 @@
+﻿#include<bits/stdc++.h>
+using namespace std;
+int n,k,cnt;
+void dfs(int last,int sum,int cur){
+    if(cur==k){//看分成的份数是否符合条件
+        if(sum==n) cnt++;//和为输入的那个数，累加方法数量
+        return;//回溯
+    }
+    for(int i=last;sum+i*(k-cur)<=n;i++){//剪枝，只用枚举到sum+i*(k-cur)<=n为止
+    //k-cur是求出还能分几份
+    //把sum+(k-cur)作为分界线，是因为当到了这一步的时候，代表后面几位数全是一样的了
+    //再往后取值的话无非是重复，就比如4，分到22就顶天了，因为再往后就是13，但是31前面搜索过了，所有跳过，剩下的不走了
+        dfs(i,sum+i,cur+1);//第一种方法就是列举k个1，然后再回溯
+    }
+    return;
+}
+int main(){
+    cin>>n>>k;//输入数字和要分成的份数
+    dfs(1,0,0);//深搜
+    cout<<cnt;
+}
