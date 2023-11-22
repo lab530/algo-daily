3d9d1c769dc575a53a2f2932f00980677784464a
xfwmhxx <2015870900@qq.com>
2023-11-22 21:37:58 +0800

    洪想想：洛谷 1794
    # 装备运输
    
    ## 题目描述
    
    德国放松对英国的进攻后，把矛头指向了东边——苏联。$1943$ 年初，东线的战斗进行到白热化阶段。据可靠情报，$90$ 余万德国军队在库尔斯克准备发动浩大攻势。因此，朱可夫元帅要求你立即从远东的军工厂运输大量装备支援库尔斯克前线。列车司机告诉你，一趟列车最多可以容纳 $V$ 体积的武器装备，但是你可能不能装满，因为列车承受不了那么大的重量，一趟列车最多可以承载 $C$ 单位的重量。同时，军工厂仓库提供给你一份装备清单，详细记录了每件装备的体积、重量和火力。为了有效支援朱可夫元帅，你要找到一种方案，使得总火力值最大。
    
    ## 输入格式
    
    第一行两个整数 $V$ 和 $G$，表示最大体积和重量。
    
    第二行一个整数 $N$，表示仓库有 $N$ 件装备。
    
    第三到 $N+2$ 行，每行 $3$ 个数 $T_i,V_i,G_i$，表示各装备的火力值、体积和重量。
    
    ## 输出格式
    
    输出一个数，表示可能获得的最大火力值。
    
    ## 样例 #1
    
    ### 样例输入 #1
    
    ```
    6 5
    4
    10 2 2
    20 3 2
    40 4 3
    30 3 3
    ```
    
    ### 样例输出 #1
    
    ```
    50
    ```
    
    ## 提示
    
    对于 $100\%$ 的数据，$1\le V, G, N \le 500$。

---
+++ 洪想想/2023/11/装备运输.cpp
@@ -0,0 +1,28 @@
+﻿#include<bits/stdc++.h>
+using namespace std;
+int ku[505][505];//创建三维数组作为背包
+struct thing{
+    int t,v,g;
+}zd[505];
+int main(){
+    int v,g,n,t,maxv,maxg;
+    cin>>maxv>>maxg;//表示最大体积和重量
+    cin>>n;//表示仓库有多少个装备
+    for(int i=1;i<=n;i++){
+        cin>>zd[i].t>>zd[i].v>>zd[i].g;
+    }
+    //ku[1][1][1]=-INT_MAX;
+    for(int i=1;i<=n;i++){
+        for(int j=maxv;j>=1;j--){
+            for(int k=maxg;k>=1;k--){
+                if(j-zd[i].v>=0 && k-zd[i].g>=0){
+                    ku[j][k]=max(ku[j][k],ku[j-zd[i].v][k-zd[i].g]+zd[i].t);
+                }else{
+                    ku[j][k]=ku[j][k];
+                }
+            } 
+        }
+    }
+    cout<<ku[maxv][maxg]<<endl;
+    return 0;
+}
