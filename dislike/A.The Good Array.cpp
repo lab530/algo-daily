530bb2db482bf31249154b7148fde7ad07eb8037
bfs1201 <146979061+bfs1201@users.noreply.github.com>
2023-11-24 02:12:02 +0800

    Codeforces Round 876 (Div. 2)
    数学规律

---
+++ dislike/A.The Good Array.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+using namespace std;
+void solve() {
+    int n, k; cin >> n >> k; 
+    double temp = (n * 1.0) / k; 
+    int ans; 
+    // 向上取整
+    ans = ceil(temp); 
+    if(n % k != 1 && k != 1) ans += 1; 
+    cout << ans << endl; 
+} 
+int main() {
+    int t; cin >> t; 
+    while(t--) {
+        solve(); 
+    }
+    return 0; 
+}
+/*
+3 2
+101
+5 2
+10101
+9 3
+100100100 -> 100100101
+7 1
+1111111
+10 4
+1000100010 -> 1000100011
+9 5
+100001000 -> 100001001
+8 8
+10000000 -> 10000001
+*/
