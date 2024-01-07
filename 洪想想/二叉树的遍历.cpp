3fe86699f15d0550bc386db0e06be758986d4141
xfwmhxx <2015870900@qq.com>
2024-01-08 01:12:39 +0800

    洪想想：洛谷：3642
    https://www.luogu.com.cn/problem/B3642

---
+++ 洪想想/二叉树的遍历.cpp
@@ -0,0 +1,78 @@
+﻿#include<bits/stdc++.h>
+using namespace std;
+int  n;
+struct kkk{
+    int left;
+    int right;
+}ku[1000005];
+//求前序序列的递归，深搜
+int bfs1(int w){
+    if(ku[w].left != 0){
+        cout << ku[w].left << " ";
+        bfs1(ku[w].left);
+    }
+    if(ku[w].right != 0){
+        cout << ku[w].right << " ";
+        bfs1(ku[w].right);
+    }
+    return 0;
+}
+//求中序序列的递归，深搜
+int bfs2(int w){
+    if(ku[w].left != 0){
+        bfs2(ku[w].left);
+        //cout << ku[w].left << " ";
+    }
+    cout << w << " ";
+    if(ku[w].right != 0){
+        bfs2(ku[w].right);
+        //cout << ku[w].right << " ";
+    }
+    return 0;
+}
+//求后序的
+int bfs3(int w){
+    if(ku[w].left != 0){
+        bfs3(ku[w].left);
+        cout << ku[w].left << " ";
+    }
+    if(ku[w].right != 0){
+        bfs3(ku[w].right);
+        cout << ku[w].right << " ";
+    }
+    //cout << w << " ";
+    return 0;
+}
+void front(){
+    cout << 1 << " ";
+    bfs1(1);
+    cout << endl;
+    return ;
+}
+
+void middle(){
+    bfs2(1);
+    cout << endl;
+    return ;
+}
+
+void beside(){
+    bfs3(1);
+    cout << 1 << " ";
+    cout << endl;
+    return ;
+}
+void slove(){
+    for(int i = 1; i <= n; i++){
+        cin >> ku[i].left >> ku[i].right;
+    }
+    front();
+    middle();
+    beside();
+}
+int main(){
+    
+    cin >> n;
+    slove();
+    return 0;
+}
